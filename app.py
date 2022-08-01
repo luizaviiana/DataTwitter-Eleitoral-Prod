5a35c04330b40b7c855c2044a5cb4097555f0675
Luiza Viana <88721668+luizaviiana@users.noreply.github.com>
2022-07-31 22:02:00 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import streamlit as st
+from PIL import Image
+
+image = Image.open("analise.jpg")
+st.image(image)
+
+st.title('Data Twitter Eleitoral')
+st.text('Aplização que realiza análise de sentimentos dos candidatos à presidência usando dados do Twitter')
+
+df = pd.read_csv("etapa4_dataframe_final.csv")
+
+candidato_unico = sorted(df['Candidato'].unique())
+selecionar_candidato = st.sidebar.multiselect('Candidato', candidato_unico, candidato_unico)
+
+df_candidato_selecionado = df[(df['Candidato'].isin(selecionar_candidato))]
+
+st.dataframe(df_candidato_selecionado)
+
+
+# utilização do streamlit cloud p colocar no servidor
